b5504abb2a05e57ea9b946339a5c070bb7c81858
wirdani84 <wirdaniyusoff@gmail.com>
2025-05-04 10:08:23 -0500

    Thirtythree Commit

--- public/script.js
+++ public/script.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const twitterBtn = document.querySelector('.twitter-btn');
-  if (twitterBtn) {
-    twitterBtn.addEventListener('click', function (event) {
-      event.preventDefault();
-      alert('Twitter OAuth login would be implemented here');
-    });
   }
 
   const learnMoreBtn = document.querySelector('.learn-more-btn');
